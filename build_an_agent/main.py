893c0031686afc6bdeed8daa1e7248a863a76409
Naomi Loughlin <Naomi.Loughlin@ibm.com>
2026-04-06 16:15:49 -0400

    create model instance and track tokens

---
+++ build_an_agent/main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if response.usage_metadata: 
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
+else:
+    raise RuntimeError("API request liekly failed")
+
+def main():
+    print("Hello from build-an-agent!")
+
+
+if __name__ == "__main__":
+    main()
